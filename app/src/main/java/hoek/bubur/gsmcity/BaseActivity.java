d89e889e0776aeafd56e06fb856cf216021c9440
alkaaf <alfa.alkaaf@gmail.com>
2017-05-19 23:01:42 +0700

    Initial commit

---
+++ app/src/main/java/hoek/bubur/gsmcity/BaseActivity.java
@@ -0,0 +1,8 @@
+package hoek.bubur.gsmcity;
+
+/**
+ * Created by dalbo on 5/19/2017.
+ */
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/hoek/bubur/gsmcity/WebService/OkFetcher.java
@@ -0,0 +1,8 @@
+package hoek.bubur.gsmcity.WebService;
+
+/**
+ * Created by dalbo on 5/19/2017.
+ */
+
+public class OkFetcher {
+}
